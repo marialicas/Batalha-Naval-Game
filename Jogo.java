d4e6beb5daa9c71a8480357949496ace3c174cc6
marialicas <eialicag@gmail.com>
2025-03-14 17:50:06 -0300

    criando a classe Jogo

---
+++ Jogo.java
@@ -0,0 +1,12 @@
+public class Jogo {
+  public static void main(String[] args) {
+    Tabuleiro tabuleiro = new Tabuleiro();
+    // Navios portaAviao = new Navios("PortaAvião", 5, 1);
+    // Navios encouracados = new Navios("Encouraçados", 4, 2);
+    // Navios submarinos = new Navios("Submarino", 1, 4);
+    // Navios cruzadores = new Navios("Cruzadores", 2, 3);
+    // Navios hidroavioes = new Navios("Hidroaviões", 3, 3);
+
+    tabuleiro.ver();
+  }
+}
